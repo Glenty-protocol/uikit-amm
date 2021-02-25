eb264b204653e3899c5c0ce21b5f714e6507bc14
devstar20 <ninjadev2020@gmail.com>
2021-02-25 21:44:33 +0200

    dist update

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
-  primaryBright: "#aa8929",
-  primaryDark: "#aa8929",
-  secondary: "#7645D9",
+  primary: "#ef7077",
+  primaryBright: "#ef7077",
+  primaryDark: "#ef7077",
+  secondary: "#ef7077",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -36,7 +36,7 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#9A6AFF",
+  secondary: "#ef7077",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
